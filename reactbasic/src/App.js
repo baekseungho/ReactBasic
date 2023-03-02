3781d6d7a79fc40cce139b83dfea101ecac2ea64
baekseungho <vudrkd1580@naver.com>
2023-03-02 11:18:38 +0900

    first

---
+++ reactbasic/src/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1></h1>
+      <h2>2341412</h2>
+    </div>
+  );
+}
+
+export default App;
