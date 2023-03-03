bd77e591f3dafd1072581d4a326a8ce69ca7c0ac
baekseungho <vudrkd1580@naver.com>
2023-03-03 18:15:43 +0900

    개념이해

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 function App() {
   let user = "SeungHo";
   let [count, setCount] = useState(0);
+  let [like, setLike] = useState([0, 0, 0]);
   //변수에 들어있는 데이터 html에 넣기
   // JSX 중괄호 문법 사용
   // 한 곳에다가 여러가지 자료를 저장하고 싶을 때 Array[ ]
@@ -13,16 +14,37 @@ function App() {
     "DW아카데미 501호",
     "DW아카데미 203호",
   ]);
+  let [myName1, setName1] = useState(["백승호1", "백승호2", "백승호3"]);
+  let [myName, setName] = useState(["백승호", "白承昊", "Bai Cheung Hao"]);
   let [bgColor, setBgc] = useState("");
   let changeBg = () => {
     let newBg = bgColor == "white" ? "red" : "white";
     setBgc(newBg);
   };
+
+  const [name2, setName2] = useState(["yejun", "lee", "lala"]);
+
+  // setName2(copy2);
+
+  // map함수가 array의 갯수만큼 return을 반복한다.
+  // 매개변수(파라미터)를 만들어주면 함수 파라미터가 array자료 안에 있는 데이터가 된다.
+  [1, 2, 3].map(function (num) {
+    // return console.log(num);
+  });
+  let num = [1, 2, 3, 4, 5];
+  let newArr = num.map((element) => {
+    return element * 3;
+  });
+  console.log(newArr);
+  // let newNum = num.map((num) => num + 1);
+  // let newNum1 = num.map((num) => num * 3);
+  // console.log(num);
+  // console.log(newNum);
+  // console.log(newNum1);
   return (
     <div className="App" style={{ backgroundColor: bgColor }}>
       <h1>Hello, {user} !</h1>
       <p>This is a React App</p>
-
       <div>
         <button
           onClick={() => {
@@ -33,18 +55,45 @@ function App() {
         </button>
         <h1>클릭시 + {count}</h1>
       </div>
-      <div className="list">
-        <h4>{title[0]}</h4>
-        <span
-          onClick={() => {
-            setCount(count - 1);
-          }}
-        >
-          💣
-        </span>
-        <p>안녕하세요. 저는 백승호 입니다.</p>
-      </div>
-      <div className="list">
+      {title.map((list, index) => {
+        return (
+          <div key={index} className="list">
+            <h4>{list}</h4>
+            {/* <h4>{title[index]}</h4> */}
+            <span
+              onClick={() => {
+                let copy = [...like];
+                copy[index] = like[index] + 1;
+                setLike(copy);
+                console.log(copy);
+              }}
+            >
+              👍 {like[index]}
+            </span>
+            <p>안녕하세요. 저는 누굴까요?</p>
+            <h5>안녕하세요. 저는 {myName1[index]}입니다.</h5>
+            <button
+              onClick={() => {
+                let copy = [...myName];
+                // setLike(copy[index]);
+                console.log(copy[index]);
+
+                const texts = document.querySelectorAll(".list p");
+                texts[
+                  index
+                ].innerHTML = `안녕하세요. 저는 ${copy[index]}입니다`;
+              }}
+            >
+              이름찾기
+            </button>
+          </div>
+        );
+      })}
+      {["yejin", "lee", "lala"].map((e) => {
+        return e.toUpperCase();
+      })}
+
+      {/* <div className="list">
         <h4>{title[1]}</h4>
         <span
           onClick={() => {
@@ -65,7 +114,16 @@ function App() {
           💣
         </span>
         <p>안녕하세요. 저는 백승호 입니다.</p>
-      </div>
+      </div> */}
+      <button
+        onClick={() => {
+          let copy = [...myName];
+          setName1(copy);
+          console.log(copy);
+        }}
+      >
+        이름변경
+      </button>
       <button
         onClick={() => {
           let copy = [...title];
@@ -111,6 +169,19 @@ function App() {
       >
         배경색 변경
       </button>
+      <button
+        onClick={() => {
+          const copy2 = [...name2];
+          copy2.map((e, index) => {
+            const change = copy2[index].toUpperCase();
+            copy2[index] = change;
+            setName2(copy2);
+          });
+        }}
+      >
+        변경
+      </button>
+      {name2}
     </div>
   );
 }
